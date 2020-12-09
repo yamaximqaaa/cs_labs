c8789d30c65a60a6c810b83a3107c3b7104ccce2
yamaximqaaa <58917435+yamaximqaaa@users.noreply.github.com>
2020-12-09 16:40:16 +0200

    add second lab

---
+++ CSharp_Net_module1_1_3_lab/Hello_Operators_advstud/Hello_Operators_advstud/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Operators_advstud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MyMax = 200;
+
+            Random random = new Random();
+            // random.Next(MaxValue) returns a 32-bit signed integer that is greater than or equal to 0 and less than MaxValue
+            int Guess_number = random.Next(MyMax) + 1;
+            // implement input of number and comparison result message in the while circle with  comparison condition
+        }
+    }
+}
---
+++ CSharp_Net_module1_1_3_lab/Hello_Operatorss_stud/Hello_Operators/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOperators_stud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long a;
+            Console.WriteLine(@"Please,  type the number:
+            1. Farmer, wolf, goat and cabbage puzzle
+            2. Console calculator
+            3. Factirial calculation
+            ");
+            
+            a = long.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    Farmer_puzzle();
+                    Console.WriteLine("");
+                    break;
+                case 2:
+                    Calculator();
+                    Console.WriteLine("");
+                    break;
+                case 3:
+                    Factorial_calculation();
+                    Console.WriteLine("");
+                    break;
+                default:
+                    Console.WriteLine("Exit");
+                    break;
+            }
+            Console.WriteLine("Press any key");
+            Console.ReadLine();
+        }
+        #region farmer
+        static void Farmer_puzzle()
+        {
+            //Key sequence: 3817283 or 3827183
+            // Declare 7 int variables for the input numbers and other variables
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(@"From one bank to another should carry a wolf, goat and cabbage. 
+At the same time can neither carry nor leave together on the banks of a wolf and a goat, 
+a goat and cabbage. You can only carry a wolf with cabbage or as each passenger separately. 
+You can do whatever how many flights. How to transport the wolf, goat and cabbage that all went well?");
+            Console.WriteLine("There: farmer and wolf - 1");
+            Console.WriteLine("There: farmer and cabbage - 2");
+            Console.WriteLine("There: farmer and goat - 3");
+            Console.WriteLine("There: farmer  - 4");
+            Console.WriteLine("Back: farmer and wolf - 5");
+            Console.WriteLine("Back: farmer and cabbage - 6");
+            Console.WriteLine("Back: farmer and goat - 7");
+            Console.WriteLine("Back: farmer  - 8");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Please,  type numbers by step ");
+            // Implement input and checking of the 7 numbers in the nested if-else blocks with the  Console.ForegroundColor changing
+
+        }
+        #endregion
+
+        #region calculator
+        static void Calculator()
+        {
+            // Set Console.ForegroundColor  value
+            Console.WriteLine(@"Select the arithmetic operation:
+1. Multiplication 
+2. Divide 
+3. Addition 
+4. Subtraction
+5. Exponentiation ");
+            // Implement option input (1,2,3,4,5)
+            //     and input of  two or one numbers
+            //  Perform calculations and output the result 
+        }
+        #endregion
+
+        #region Factorial
+        static void Factorial_calculation()
+        {
+            // Implement input of the number
+            // Implement input the for circle to calculate factorial of the number
+            // Output the result
+        }
+        #endregion
+    }
+}
