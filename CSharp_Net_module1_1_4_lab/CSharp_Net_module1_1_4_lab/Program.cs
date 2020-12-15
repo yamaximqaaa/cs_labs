6890b6b684446f5d955361de0645b382a8d33bd8
yamaximqaaa <58917435+yamaximqaaa@users.noreply.github.com>
2020-12-15 18:26:40 +0200

    12

---
+++ CSharp_Net_module1_1_4_lab/CSharp_Net_module1_1_4_lab/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_1_4_lab
+{
+    class Program
+    {
+        // 1) declare enum ComputerType
+        enum typePC
+        {
+            desktop,
+            laptop,
+            server
+        }
+
+        // 2) declare struct Computer
+        struct computer
+        {
+            public typePC type;
+            public int CPUc;
+            public double CPUh;
+            public int memory;
+            public int HDD;
+
+            public computer(typePC type_, 
+                     int CPUc_, 
+                     double CPUh_, 
+                     int memory_, 
+                     int HDD_)
+            {
+                type = type_;
+                CPUc = CPUc_;
+                CPUh = CPUh_;
+                memory = memory_;
+                HDD = HDD_;
+            }
+            
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            // 3) declare jagged array of computers size 4 (4 departments)
+            computer[][] dep = new computer[4][];
+
+
+            // 4) set the size of every array in jagged array (number of computers)
+            computer desktop = new computer(typePC.desktop, 4, 2.5, 6, 500);
+            computer laptop = new computer(typePC.laptop, 2, 1.7, 4, 250);
+            computer server = new computer(typePC.server, 8, 3, 16, 2000);
+            dep[0] = new computer[5]; //{ desktop, desktop, laptop, laptop, server };
+            dep[1] = new computer[3]; //{ laptop, laptop, laptop };
+            dep[2] = new computer[5]; //{ desktop, desktop, laptop, laptop, desktop }; 
+            dep[3] = new computer[4]; //{ desktop,  laptop, server, server }; 
+
+            // 5) initialize array
+            // Note: use loops and if-else statements
+            
+            //computer desktop = new computer(typePC.desktop, 4, 2.5, 6, 500);
+            //computer laptop = new computer(typePC.laptop, 2, 1.7, 4, 250);
+            //computer server = new computer(typePC.server, 8, 3, 16, 2000);
+
+            
+            int input = 0;
+            for (int i = 0; i < dep.GetLength(0); i++)
+            {
+                Console.WriteLine("Departament {0}.", i+1);
+                for (int j = 0; j < dep[i].GetLength(0); j++)
+                {
+                    Console.WriteLine("What computer you want to add?");
+                    Console.WriteLine("1 -> Desktop");
+                    Console.WriteLine("2 -> Laptop");
+                    Console.WriteLine("3 -> Server");
+                    bool check = false;
+                    while (!check)
+                    {
+                        input = int.Parse(Console.ReadLine());
+                        switch (input)
+                        {
+                            case 1:
+                                dep[i][j] = desktop;
+                                check = true;
+                                Console.WriteLine();
+                                break;
+                            case 2:
+                                dep[i][j] = laptop;
+                                check = true;
+                                Console.WriteLine();
+                                break;
+                            case 3:
+                                dep[i][j] = server;
+                                check = true;
+                                Console.WriteLine();
+                                break;
+                            default:
+                                Console.WriteLine("Incorrect input.");
+                                check = false;
+                                break;
+                        }
+                    }
+                }
+                Console.Clear();
+            }
+
+            // 6) count total number of every type of computers
+            // 7) count total number of all computers
+            // Note: use loops and if-else statements
+            // Note: use the same loop for 6) and 7)
+            int allComp = 0,
+                desktopCount = 0,
+                laptopCount = 0,
+                serverCount = 0;
+            
+            for (int i = 0; i < dep.GetLength(0); i++)
+            {
+                for (int j = 0; j < dep[i].GetLength(0); j++)
+                {
+                    allComp++;
+                    if (dep[i][j].type == typePC.desktop)
+                        desktopCount++;
+                    if (dep[i][j].type == typePC.laptop)
+                        laptopCount++;
+                    if (dep[i][j].type == typePC.server)
+                        serverCount++;
+                }
+            }
+            Console.WriteLine("All computers: {0}", allComp);
+            Console.WriteLine("Desktop computers: {0}", desktopCount);
+            Console.WriteLine("Laptop computers: {0}", laptopCount);
+            Console.WriteLine("Server computers: {0}", serverCount);
+            Console.WriteLine();
+            // 8) find computer with the largest storage (HDD) - 
+            // compare HHD of every computer between each other; 
+            // find position of this computer in array (indexes)
+            // Note: use loops and if-else statements
+            computer largestHDD = dep[0][0];
+            int[] ID = new int[2];
+            for (int i = 0; i < dep.GetLength(0); i++)
+            {
+                for (int j = 0; j < dep[i].GetLength(0); j++)
+                {
+                    if (dep[i][j].HDD >= largestHDD.HDD)
+                    {
+                        largestHDD = dep[i][j];
+                        ID[0] = i;
+                        ID[1] = j;
+                    }
+                }
+            }
+
+            Console.WriteLine("Size of largest HDD is: {0}.", largestHDD.HDD);
+            Console.WriteLine("ID: [{1}][{2}]", ID[0], ID[1]);
+            Console.WriteLine();
+            // 9) find computer with the lowest productivity (CPU and memory) - 
+            // compare CPU and memory of every computer between each other; 
+            // find position of this computer in array (indexes)
+            // Note: use loops and if-else statements
+            // Note: use logical oerators in statement conditions
+            computer slovestComp = dep[0][0];
+            for (int i = 0; i < dep.GetLength(0); i++)
+            {
+                for (int j = 0; j < dep[i].GetLength(0); j++)
+                {
+                    if (dep[i][j].CPUc <= slovestComp.CPUc &&
+                        dep[i][j].CPUh <= slovestComp.CPUh &&
+                        dep[i][j].memory <= slovestComp.memory )
+                    {
+                        slovestComp = dep[i][j];
+                        ID[0] = i;
+                        ID[1] = j;
+                    }
+                }
+            }
+            Console.WriteLine("CPU of lowest productivity PC is: {0} cores, {1} HGz. Memory is {2}GB", slovestComp.CPUc, 
+                slovestComp.CPUh, slovestComp.memory);
+            Console.WriteLine("ID: [{0}][{1}]", ID[0], ID[1]);
+            Console.WriteLine();
+            // 10) make desktop upgrade: change memory up to 8
+            // change value of memory to 8 for every desktop. Don't do it for other computers
+            // Note: use loops and if-else statements
+
+            
+            for (int i = 0; i < dep.GetLength(0); i++)
+            {
+                for (int j = 0; j < dep[i].GetLength(0); j++)
+                {
+                    if (dep[i][j].type == typePC.desktop)
+                    {
+                        dep[i][j].memory = 8;
+                        Console.WriteLine("Upgrade sucsessful!!!");
+                    }
+                }
+            }
+            
+        }
+ 
+    }
+}
---
+++ CSharp_Net_module1_1_5_lab/Hello_Console_stud/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Threading;
+
+namespace Hello_Console_stud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            try
+            {
+                do
+                {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine(@"Please,  type the number:
+                    1.  f(a,b) = |a-b| (unary)
+                    2.  f(a) = a (binary)
+                    3.  music
+                    4.  morse sos
+          
+                    ");
+                    try
+                    {
+                        a = (int)uint.Parse(Console.ReadLine());
+                        switch (a)
+                        {
+                            case 1:
+                                My_strings();
+                                Console.WriteLine("");
+                                break;
+                            case 2:
+                                My_Binary();
+                                Console.WriteLine("");
+                                break;
+                            case 3:
+                                My_music();
+                                Console.WriteLine("");
+                                break;
+                            case 4:
+                                Morse_code();
+                                Console.WriteLine("");
+                                break;                   
+                            default:
+                                Console.WriteLine("Exit");
+                                break;
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Error"+e.Message);
+                    }                   
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Press Spacebar to exit; press any key to continue");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                while (Console.ReadKey().Key != ConsoleKey.Spacebar);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        #region ToFromBinary
+        static void My_Binary()
+        {
+            //Implement positive integer variable input
+
+            //Present it like binary string
+            //   For example, 4 as 100
+
+            //Use modulus operator to obtain the remainder  (n % 2) 
+            //and divide variable by 2 in the loop
+
+            //Use the ToCharArray() method to transform string to chararray
+            //and Array.Reverse() method
+
+        }
+        #endregion
+
+        #region ToFromUnary
+        static void My_strings()
+        {
+           //Declare int and string variables for decimal and binary presentations
+
+           //Implement two positive integer variables input
+
+           //To present each of them in the form of unary string use for loop
+
+           //Use concatenation of these two strings 
+           //Note it is necessary to use some symbol ( for example “#”) to separate
+
+           //Check the numbers on the equality 0
+           //Realize the  algorithm for replacing '1#1' to '#' by using the for loop 
+           //Delete the '#' from algorithm result
+
+           //Output the result 
+         }
+        #endregion
+
+        #region My_music
+        static void My_music()
+        {
+            //HappyBirthday
+            Thread.Sleep(2000);
+            Console.Beep(264, 125);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(125);
+            Console.Beep(297, 500);
+            Thread.Sleep(125);
+            Console.Beep(264, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 500);
+            Thread.Sleep(125);
+            Console.Beep(330, 1000);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(125);
+            Console.Beep(297, 500);
+            Thread.Sleep(125);
+            Console.Beep(264, 500);
+            Thread.Sleep(125);
+            Console.Beep(396, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 1000);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(125);
+            Console.Beep(2642, 500);
+            Thread.Sleep(125);
+            Console.Beep(440, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 250);
+            Thread.Sleep(125);
+            Console.Beep(352, 125);
+            Thread.Sleep(125);
+            Console.Beep(330, 500);
+            Thread.Sleep(125);
+            Console.Beep(297, 1000);
+            Thread.Sleep(250);
+            Console.Beep(466, 125);
+            Thread.Sleep(250);
+            Console.Beep(466, 125);
+            Thread.Sleep(125);
+            Console.Beep(440, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 500);
+            Thread.Sleep(125);
+            Console.Beep(396, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 1000);
+        }
+        #endregion
+
+        #region Morse
+        static void Morse_code()
+        {   
+            //Create string variable for 'sos'      
+
+            //Use string array for Morse code
+            string[,] Dictionary_arr = new string [,] { { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" },
+            { ".-   ", "-... ", "-.-. ", "-..  ", ".    ", "..-. ", "--.  ", ".... ", "..   ", ".--- ", "-.-  ", ".-.. ", "--   ", "-.   ", "---  ", ".--. ", "--.- ", ".-.  ", "...  ", "-    ", "..-  ", "...- ", ".--  ", "-..- ", "-.-- ", "--.. ", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----." }};
+            //Use ToCharArray() method for string to copy charecters to Unicode character array
+            //Use foreach loop for character array in which
+
+                //Implement Console.Beep(1000, 250) for '.'
+                // and Console.Beep(1000, 750) for '-'
+
+                //Use Thread.Sleep(50) to separate sounds
+            //                  
+        }
+
+        #endregion
+    }
+}
