c12ed73278f592fa46df054ac71e617b88535a2e
yamaximqaaa <58917435+yamaximqaaa@users.noreply.github.com>
2020-12-16 18:41:45 +0200

    21

---
+++ CSharp_Net_module1_2_1_lab/CSharp_Net_module1_2_1_lab/LibraryUser.cs
@@ -0,0 +1,189 @@
+﻿using System;
+
+namespace CSharp_Net_module1_2_1_lab
+{
+
+    // 1) declare interface ILibraryUser
+    // declare method's signature for methods of class LibraryUser
+    interface ILibraryUser
+    {
+        //int CreateID();
+        //bool AddBook(string book);
+        //void RemoveBook();
+        //void BookInfo();
+        //void BookCount();
+        
+    }
+
+
+    // 2) declare class LibraryUser, it implements ILibraryUser
+    class LibraryUser : ILibraryUser
+    {
+        // 3) declare properties: FirstName (read only), LastName (read only), 
+        // Id (read only), Phone (get and set), BookLimit (read only)
+        #region private
+        string FirstName;
+        string LastName;
+        int ID;
+        string Phone;
+        int BookLimit;
+
+        
+        #endregion
+
+        // 4) declare field (bookList) as a string array
+        private string[] bookList;
+
+        // 5) declare indexer BookList for array bookList
+        private int BookList = 0;
+
+        // 6) declare constructors: default and parameter
+        public LibraryUser()
+        {
+            FirstName = null;
+            LastName = null;
+            ID = 1111;
+            Phone = "1111111111";
+            BookLimit = 0;
+        }
+
+        public LibraryUser(string FirstName, 
+                           string LastName,
+                           string Phone,
+                           int BookLimit)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            ID = CreateID();
+            this.Phone = Phone;
+            this.BookLimit = BookLimit;
+        }
+        private int CreateID()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1000, 9999);
+        }
+
+        // 7) declare methods: 
+
+        //AddBook() – add new book to array bookList
+        public void AddBook(string book)
+        {
+            // if no book we create array
+            if(BookList == 0)
+            {
+                bookList = new string[BookLimit];
+                bookList[0] = book;
+                BookList++;
+                Console.WriteLine("Book: {0}, added. ", book);
+                return;
+            }
+            // if person take book we add new to array
+            else
+            {
+                // if book limit is out
+                if (BookLimit == BookList)
+                {
+                    Console.WriteLine("Limit of books is out!");
+                    return;
+                }
+                // add new book
+                else
+                {
+                    bookList[BookList] = book;
+                    BookList++;
+                    Console.WriteLine("Book: {0}, added. ", book);
+                    return;
+                }
+            }
+        }
+
+        //RemoveBook() – remove book from array bookList
+        public void RemoveBook(int index)
+        {
+            // check empy array
+            if (BookList == 0)
+                Console.WriteLine("Array is free!");
+            else
+            {
+                for (int i = index; i < BookList; i++)
+                {
+                    // if delete last book
+                    if (i == (BookList - 1))
+                    {
+                        bookList[i] = null;
+                        BookList--;
+                        return;
+                    }
+                    // when stop swap books
+                    if (bookList[i] == null)
+                    {
+                        BookList--;
+                        return;
+                    }
+                    // swap alg
+                    else
+                    {
+                        bookList[i] = bookList[i + 1];
+                        continue;
+                    }
+                }
+            }
+            
+
+        }
+
+        //BookInfo() – returns book info by index
+        public string BookInfo(int index)
+        {
+            string bookData = bookList[index];
+            Console.WriteLine("{0}. {1}", index + 1, bookData);
+            return bookData;
+        }
+
+        //BooksCout() – returns current count of books
+        public int BooksCout()
+        {
+            // check empty array
+            if (bookList[0] == null)
+                return 0;
+            // check full array
+            if (bookList[BookList-1] != null)
+                return BookList;
+            // count books
+            else
+            {
+                int counter = 0;
+                bool marker = false;
+                while (!marker)
+                {
+                    counter++;
+                    if (bookList[counter] == null)
+                    {
+                        marker = true;
+                        break;
+                    }
+
+                }
+                return counter;
+            } 
+        }
+
+        public void PrintAllBook()
+        {
+            for (int i = 0; i < BookLimit; i++)
+            {
+                Console.WriteLine("{0}. {1}", i+1, bookList[i]);
+            }
+        }
+
+        // Geters
+
+        public string GetFirstName() { return FirstName; }
+        public string GetLastName() { return LastName; }
+        public string GetPhone() { return Phone; }
+        public int GetID() { return ID; }
+        public int GetLimit() { return BookLimit; }
+
+    }
+}
---
+++ CSharp_Net_module1_2_1_lab/CSharp_Net_module1_2_1_lab/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CSharp_Net_module1_2_1_lab
+{
+
+    class Program
+    {
+        static void MainTask()
+        {
+            // 8) declare 2 objects. Use default and paremeter constructors
+            LibraryUser user1 = new LibraryUser("Vasilii", "Peprovchich", "+380447777777", 2), user2 = new LibraryUser("Maria", "Ivanenko", "+380447777777", 2);
+            Console.WriteLine("User1 " + user1.GetFirstName() + " " + user1.GetLastName() + " " + user1.GetID());
+            Console.WriteLine("User2 " + user2.GetFirstName() + " " + user2.GetLastName() + " " + user2.GetID());
+
+            // 9) do operations with books for all users: run all methods for both objects
+            Console.WriteLine("User 1: add Harry Potter");
+            user1.AddBook("Harry Potter");
+            Console.WriteLine("User 2: add Sherlock Holmes");
+            user2.AddBook("Sherlock Holmes");
+            Console.WriteLine("user1.BooksCount = " + user1.BooksCout() + "; user2.BooksCount " + user2.BooksCout());
+            Console.WriteLine("user2 :");
+            Console.WriteLine("Add Kobzar");
+            user2.AddBook("Kobzar");
+            Console.WriteLine("user2.BooksCount " + user2.BooksCout());
+            Console.WriteLine("Add Dorian Gray");
+            user2.AddBook("Dorian Gray");
+            Console.WriteLine("user2.BooksCount " + user2.BooksCout());
+            Console.WriteLine("Remove Sherlock Holmes");
+            user2.RemoveBook(1);
+            Console.WriteLine("user2.BooksCount " + user2.BooksCout());
+        }
+        
+        void MyTests()
+        {
+            var client1 = new LibraryUser("Max", "Lukashevich", "0980703727", 5);
+
+            client1.AddBook("book1");
+            client1.AddBook("book2");
+            client1.AddBook("book3");
+            client1.AddBook("book4");
+            client1.AddBook("book5");
+
+            client1.RemoveBook(1);
+            client1.RemoveBook(0);
+            client1.RemoveBook(4);
+
+            client1.PrintAllBook();
+            Console.WriteLine("Book count: " + client1.BooksCout());
+
+
+            client1.AddBook("book1");
+            client1.AddBook("book2");
+            Console.WriteLine("Book count: " + client1.BooksCout());
+            client1.PrintAllBook();
+        }
+        static void Main(string[] args)
+        {
+            MainTask();
+
+
+        }
+    }
+}
