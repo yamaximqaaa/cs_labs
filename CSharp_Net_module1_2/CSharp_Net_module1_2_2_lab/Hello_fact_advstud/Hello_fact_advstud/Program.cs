1e90c55f24bcc9ffb2c943ebd50efa45a582d206
yamaximqaaa <58917435+yamaximqaaa@users.noreply.github.com>
2021-01-12 15:21:59 +0200

    lab1_2_2_2

--- CSharp_Net_module1_2/CSharp_Net_module1_2_2_lab/Hello_fact_advstud/Hello_fact_advstud/Program.cs
+++ CSharp_Net_module1_2/CSharp_Net_module1_2_2_lab/Hello_fact_advstud/Hello_fact_advstud/Program.cs
@@ -12,11 +12,21 @@ namespace Hello_fact_advstud
         {
             //Define parameters to calculate the factorial of
             //Call fact() method to calculate
+            var num = 6;
+            Console.WriteLine("Fact result: " + factt(num)); 
         }
 
         //Create fact() method  with parameter to calculate factorial
         //Use ternary operator
+        static int factt(int num)
+        {
+            int res = 0;
+            if (num == 1)
+                return 1;
+            else
+                return num * factt(num - 1);
 
+        }
     }
 
     
