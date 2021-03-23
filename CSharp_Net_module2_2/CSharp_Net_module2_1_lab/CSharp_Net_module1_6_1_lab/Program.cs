97f2ca805c659dfaef89347f5ea967810fb0251a
Max Mad <58917435+yamaximqaaa@users.noreply.github.com>
2021-03-23 14:52:20 +0200

    eventOut

---
+++ CSharp_Net_module2_2/CSharp_Net_module2_1_lab/CSharp_Net_module1_6_1_lab/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharp_Net_module1_6_1_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //use Console.ForegroundColor = ConsoleColor.Gray for output
+            Console.ForegroundColor = ConsoleColor.Gray;
+            //create ThreadManipulator object
+            ThreadManipulator threadManipulator = new ThreadManipulator();
+            
+            //create first thread for AddingOne method
+            Thread firstTrhreead = new Thread(new ParameterizedThreadStart(threadManipulator.AddingOne));
+            
+            //create second thread for AddingOne method
+            Thread secondTrhreead = new Thread(new ParameterizedThreadStart(threadManipulator.AddingOne));
+            
+            //create thread for AddingCustomValue method
+            Thread thirdTrhreead = new Thread(new ParameterizedThreadStart(threadManipulator.AddingCustomValue));
+            
+            //create Background thread for Stop method
+            Thread myThreadBackground = new Thread(new ThreadStart(threadManipulator.Stop));
+            myThreadBackground.IsBackground = true;
+            //start Background thread for Stop method
+            myThreadBackground.Start();
+
+            //start first thread for AddingOne method with argument = 10
+            firstTrhreead.Start(10);
+            //start second thread for AddingOne method with argument = 20
+            secondTrhreead.Start(20);
+            //start thread for AddingCustomValue method with argument new[] { 5, 15 }
+            thirdTrhreead.Start(new[] { 5, 15 });
+            //join threads
+
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ CSharp_Net_module2_2/CSharp_Net_module2_1_lab/CSharp_Net_module1_6_1_lab/ThreadManipulator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharp_Net_module1_6_1_lab
+{
+    class ThreadManipulator
+    {
+        //declare private ConsoleKey  _key  field
+        private ConsoleKey _key { get; set; }
+        //In general, avoid locking on a public type, or instances beyond your code's control. The common constructs lock (this), lock (typeof (MyType)),
+        //and lock ("myLock") violate this guideline:
+        //lock (this) is a problem if the instance can be accessed publicly.
+        //lock (typeof (MyType)) is a problem if MyType is publicly accessible.
+        //lock("myLock") is a problem because any other code in the process using the same string, will share the same lock.
+        //Best practice is to define a private object to lock on, or a private static object variable to protect data common to all instances.
+        
+        //create static readonly object _block by new operator
+
+        private static readonly object _block = new object();    //lock
+
+        //implement AddingOne(object raw) method
+        public void AddingOne(object raw)
+        {
+            var number = (int)raw;
+            //get ManagedThreadId
+            Console.WriteLine("Current thread: " + Thread.CurrentThread.ManagedThreadId);
+            //use lock block with for loop for counter % number calculation
+
+            lock (_block)          
+            {
+                for (var counter = 0; counter < 100 && _key != ConsoleKey.Q; ++counter)
+                {
+
+                    //use Console.ForegroundColor = ConsoleColor.Blue for output    
+                    //simulate the long process with Thread.Sleep
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("number % counter " + counter % number);
+                    Thread.Sleep(500);
+                }
+            }
+        }
+
+
+        //implement AddingCustomValue(object args) method
+        public void AddingCustomValue(object args)
+        {
+            //get ManagedThreadId
+            Console.WriteLine("Current thread: " + Thread.CurrentThread.ManagedThreadId);
+            //define
+            var arr = (int[])args;
+            var number = arr[0];
+            var step = arr[1];
+
+            //use for loop for counter % number calculation
+
+
+            for (var counter = 0; counter < 100 * step && _key != ConsoleKey.W; counter += step)
+            {
+
+                //use Console.ForegroundColor = ConsoleColor.Green for output
+                //simulate the long process with Thread.Sleep
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Count % number " + counter % number);
+                Thread.Sleep(500);
+            }
+
+        }
+
+        //implement Stop() method
+        public void Stop()
+        {
+            //get ManagedThreadId
+            Console.WriteLine("Current thread: " + Thread.CurrentThread.ManagedThreadId);
+            //craete while loop to read key
+            do
+            {
+                _key = Console.ReadKey().Key;
+                //use Console.ForegroundColor = ConsoleColor.Red for output
+
+                if (_key == ConsoleKey.Q)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("thread " + Thread.CurrentThread.ManagedThreadId + " aborded");
+                    Thread.CurrentThread.Abort();
+                }
+                if (_key == ConsoleKey.W)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("thread " + Thread.CurrentThread.ManagedThreadId + " aborded");
+                    Thread.CurrentThread.Abort();
+                }
+            } while (true);
+        }
+    }
+}
